614b50a46d857eeb12904e01037d155d4fe87275
吳啟佑 <wilsonwu@double-cash.com>
2023-11-09 11:00:09 +0800

    fixed image path

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import "./index.css";
 
+function getImageUrl(name) {
+  return new URL(`/src/assets/${name}.png`, import.meta.url).href;
+}
+
 const container = document.querySelector("#container");
 
 const template = `<div class="flex flex-col rounded-md p-4 shadow-xl">
@@ -17,28 +21,29 @@ const template = `<div class="flex flex-col rounded-md p-4 shadow-xl">
 const data = [
   {
     url: "./src/assets/SUNNEW.apk",
-    img: "./src/assets/sunshinetv.png",
+    img: "sunshinetv",
     name: "晴天TV",
     enName: "SunshineTV",
   },
   {
     url: "./src/assets/smarttube_stable.apk",
-    img: "./src/assets/smartyoutube.png",
+    img: "smartyoutube",
     name: "SmartYoutube",
     enName: "smartyoutube",
   },
   {
     url: "./src/assets/mudvod-TVBOX-2.0.4-NOAD-NoUpdate-GDaily.org.apk",
-    img: "./src/assets/nivod.png",
+    img: "nivod",
     name: "泥視頻",
     enName: "Nivod",
   },
 ];
 let content = "";
 for (let i = 0; i < data.length; i++) {
+  console.log(getImageUrl(data[i].img));
   content += template
     .replace("{{url}}", data[i].url)
-    .replace("{{img}}", data[i].img)
+    .replace("{{img}}", getImageUrl(data[i].img))
     .replace("{{name}}", data[i].name)
     .replace("{{enName}}", data[i].enName);
 }
